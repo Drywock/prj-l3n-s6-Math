e78547b25566a8077efc60305f2cecbbcb6164b1
ZaChr0me <48518776+ZaChr0me@users.noreply.github.com>
2020-05-28 17:50:20 +0200

    Update functions.h

--- src/functions.h
+++ src/functions.h
@@ -5,5 +5,5 @@
 
 uint64_t sumAllCombination( std::vector<uint64_t> integers);
 bool isPrime(int n);
-int gcd(uint64_t a, uint64_t b);
-bool absolute(uint64_t a);
+uint64_t gcd(uint64_t a, uint64_t b);
+uint64_t absolute(uint64_t a);
