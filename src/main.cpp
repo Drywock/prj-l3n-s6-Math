670ea6be4a9a0ad14dc7c02f204847691931aab8
Thomas LINTANF <thomas.lintanf@efrei.net>
2020-05-29 01:03:46 +0200

    Fix merge bullshit 2

--- src/main.cpp
+++ src/main.cpp
@@ -10,46 +10,12 @@
 #define MAX_N 1000000
 
 int main() {
-<<<<<<< HEAD
 	TIME_INIT_SESSION("Global timing");
-	TIME(main);
-	/*
-	OutputEventHandler out;
-	out.writeDataToBuffer(3, "test");
-	*/
-	std::thread t(OutputEventHandler::worker);
-	for (uint64_t n = 2; n <= MAX_N; n++) {
-		std::vector<uint64_t> uns;
-		uint64_t result = n;
-		uint64_t oldResult = 0;
-
-		while (result != oldResult)
-		{
-			oldResult = result;
-			result = f_rho_Pollard(oldResult);
-			uns.push_back(result);
-		}
-
-		std::stringstream result_str;
-		result_str << n;
-		for (auto un : uns) {
-			result_str << ";" << un;
-		}
-		OutputEventHandler::writeDataToBuffer(OutputManager::Levels::LOG_CONSOLE, result_str.str());
-	}
-	OutputEventHandler::stopWorker();
-	t.join();
-	TIME_END_SESSION;
-=======
-    TIME_INIT_SESSION("Global timing");
-    {
+	
+    {   
         TIME(main);
-        /*
-        OutputEventHandler out;
-        out.writeDataToBuffer(3, "test");
-        */
-
-        for(uint64_t n = 2; n <= MAX_N; n++) {
+        std::thread t(OutputEventHandler::worker);
+        for (uint64_t n = 2; n <= MAX_N; n++) {
             std::vector<uint64_t> uns;
             uint64_t result = n;
             uint64_t oldResult = 0;
@@ -63,14 +29,13 @@ int main() {
 
             std::stringstream result_str;
             result_str << n;
-            for(auto un : uns) {
+            for (auto un : uns) {
                 result_str << ";" << un;
             }
-            result_str << "\n";
-            std::cout << result_str.str() ;
-            // out.writeDataToBuffer(4, result_str.str());
+            OutputEventHandler::writeDataToBuffer(OutputManager::Levels::LOG_CONSOLE, result_str.str());
         }
+        OutputEventHandler::stopWorker();
+        t.join();
     }
-    TIME_END_SESSION;
->>>>>>> f105cc508ac88f2914a2f565758fd75178f552f1
+	TIME_END_SESSION;
 }
