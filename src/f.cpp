bab470cddd68908ee5350f78111990b42e7eead6
Thomas LINTANF <thomas.lintanf@efrei.net>
2020-05-28 20:07:00 +0200

    It's starting to work

--- src/f.cpp
+++ src/f.cpp
@@ -1,19 +1,25 @@
 #include "f.h"
 
 #include <map>
+#include <iostream>
 
 #include "functions.h"
 #include "rhoPollard.h"
+#include "time/time.h"
+
+std::map<uint64_t, uint64_t> *g_results = new std::map<uint64_t, uint64_t>;
 
 uint64_t f_rho_Pollard(const uint64_t &n) {
-    static std::map<uint64_t, uint64_t> results;
-    try {
-        return results.at(n);
-    } catch(std::out_of_range e) {
-        RhoPollard r;
-        std::vector<uint64_t> factors = r.getFactors(n);
-        int64_t result = sumAllCombination(factors);
-        results.insert(std::pair<uint64_t, uint64_t>(n, result));
-        return result;
+    TIME(f_rho_Pollard);
+    int64_t result;
+    
+    if(g_results->count(n)) {
+        return (*g_results)[n];
     }
+
+    RhoPollard r;
+    std::vector<uint64_t> factors = r.getFactors(n);
+    result = sumAllCombination(factors);
+    (*g_results)[n] = result;
+    return result;
 }
--- src/main.cpp
+++ src/main.cpp
@@ -5,29 +5,36 @@
 #include "output/outputEventHandler.h"
 #include "f.h"
 
-#define MAX_N 100
+#define MAX_N 1000000
 
 int main() {
     TIME_INIT_SESSION("Global timing");
+    TIME(main);
+    /*
     OutputEventHandler out;
+    out.writeDataToBuffer(3, "test");
+    */
 
-    for(uint64_t n = 2; n < MAX_N; n++) {
+    for(uint64_t n = 2; n <= MAX_N; n++) {
         std::vector<uint64_t> uns;
         uint64_t result = n;
-        do
-        {
-            result = f_rho_Pollard(result);
-            uns.push_back(result);
-        } while (result != n);
+        uint64_t oldResult = 0;
 
+        while (result != oldResult)
+        {
+            oldResult = result;
+            result = f_rho_Pollard(oldResult);
+            uns.push_back(result);
+        }
 
         std::stringstream result_str;
         result_str << n;
         for(auto un : uns) {
             result_str << ";" << un;
         }
-
-        out.writeDataToBuffer(4, result_str.str());
+        result_str << "\n";
+        std::cout << result_str.str() ;
+        // out.writeDataToBuffer(4, result_str.str());
     }
     TIME_END_SESSION;
 }
