f105cc508ac88f2914a2f565758fd75178f552f1
Thomas LINTANF <thomas.lintanf@efrei.net>
2020-05-28 20:26:40 +0200

    Fix timing

--- src/main.cpp
+++ src/main.cpp
@@ -9,32 +9,34 @@
 
 int main() {
     TIME_INIT_SESSION("Global timing");
-    TIME(main);
-    /*
-    OutputEventHandler out;
-    out.writeDataToBuffer(3, "test");
-    */
+    {
+        TIME(main);
+        /*
+        OutputEventHandler out;
+        out.writeDataToBuffer(3, "test");
+        */
 
-    for(uint64_t n = 2; n <= MAX_N; n++) {
-        std::vector<uint64_t> uns;
-        uint64_t result = n;
-        uint64_t oldResult = 0;
+        for(uint64_t n = 2; n <= MAX_N; n++) {
+            std::vector<uint64_t> uns;
+            uint64_t result = n;
+            uint64_t oldResult = 0;
 
-        while (result != oldResult)
-        {
-            oldResult = result;
-            result = f_rho_Pollard(oldResult);
-            uns.push_back(result);
+            while (result != oldResult)
+            {
+                oldResult = result;
+                result = f_rho_Pollard(oldResult);
+                uns.push_back(result);
+            }
+
+            std::stringstream result_str;
+            result_str << n;
+            for(auto un : uns) {
+                result_str << ";" << un;
+            }
+            result_str << "\n";
+            std::cout << result_str.str() ;
+            // out.writeDataToBuffer(4, result_str.str());
         }
-
-        std::stringstream result_str;
-        result_str << n;
-        for(auto un : uns) {
-            result_str << ";" << un;
-        }
-        result_str << "\n";
-        std::cout << result_str.str() ;
-        // out.writeDataToBuffer(4, result_str.str());
     }
     TIME_END_SESSION;
 }
