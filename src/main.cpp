eac7ba318f411bcfc3f7da35605dd7956d4fdaa5
Thomas LINTANF <thomas.lintanf@efrei.net>
2020-05-29 00:59:22 +0200

    Fix merge bullshit

--- src/main.cpp
+++ src/main.cpp
@@ -10,37 +10,6 @@
 #define MAX_N 1000000
 
 int main() {
-<<<<<<< HEAD
-	TIME_INIT_SESSION("Global timing");
-	TIME(main);
-	/*
-	OutputEventHandler out;
-	out.writeDataToBuffer(3, "test");
-	*/
-	std::thread t(OutputEventHandler::worker);
-	for (uint64_t n = 2; n <= MAX_N; n++) {
-		std::vector<uint64_t> uns;
-		uint64_t result = n;
-		uint64_t oldResult = 0;
-
-		while (result != oldResult)
-		{
-			oldResult = result;
-			result = f_rho_Pollard(oldResult);
-			uns.push_back(result);
-		}
-
-		std::stringstream result_str;
-		result_str << n;
-		for (auto un : uns) {
-			result_str << ";" << un;
-		}
-		OutputEventHandler::writeDataToBuffer(OutputManager::Levels::LOG_CONSOLE, result_str.str());
-	}
-	OutputEventHandler::stopWorker();
-	t.join();
-	TIME_END_SESSION;
-=======
     TIME_INIT_SESSION("Global timing");
     {
         TIME(main);
@@ -72,5 +41,4 @@ int main() {
         }
     }
     TIME_END_SESSION;
->>>>>>> f105cc508ac88f2914a2f565758fd75178f552f1
 }
